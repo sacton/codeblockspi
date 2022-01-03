0f063d2eb69ca5ce32c6ddb8753943344ecca6cf
Steve <steve@cton.me>
2022-01-02 22:07:15 -0600

    "Hallo!"

--- commit.sh
+++ commit.sh
@@ -1,6 +1,23 @@
-#!/usr/bin/expect -f
+#!/bin/bash
+#to make a directory the local git:
+# git init
+# git remote add origin https://github.com/name/project.git
+# git-ignore ./anystufftoignore
 
-spawn ./gitcmds.sh
+#add all files in this folder to the git index
+git add -A
 
-expect "Username for"
-send--"sacton\r"
+#put a comment on the commit
+echo Enter a comment for commit
+read comment
+git commit -m \""$comment\""
+
+echo
+echo the key is
+echo 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
+echo
+
+#update remote branch with commit
+git push origin HEAD:main
+
+
