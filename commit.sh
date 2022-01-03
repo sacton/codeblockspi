f542caa79722271fbb50110d7d843d0c6f4d3afa
Steve <steve@cton.me>
2022-01-02 21:45:55 -0600

    "This is a comment"

--- commit.sh
+++ commit.sh
@@ -1,8 +1,16 @@
-#add all files to the git index
+#!/bin/bash
+#to make a directory the local git:
+# git init
+# git remote add origin https://github.com/name/project.git
+# git-ignore ./anystufftoignore
+
+#add all files in this folder to the git index
 git add -A
 
 #put a comment on the commit
-git commit -m "This is a comment in a script"
+echo Enter a comment for commit
+read comment
+git commit -m \""$comment\""
 
 #update remote branch with commit
 git push origin HEAD:main
--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main()
 {
 
     for (int i = 0; i<10; i++){
-
+        //this is a comment
         cout << "Hello, World! This is loop number " << i << endl;
    }
     return 0;
